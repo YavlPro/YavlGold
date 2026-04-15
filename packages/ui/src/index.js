f3cded60150f0cfa985757d9ab0925db4f33be1b
Yerikson Varela <tu_correo@ejemplo.com>
2026-04-15 09:07:50 -0400

    chore(repo): remove legacy zombie packages and ghost copies

--- packages/ui/src/index.js
+++
@@ -1,7 +0,0 @@
-// @yavl/ui - Index de exports principales
-// Componentes UI compartidos del ecosistema Yavl
-
-export { Modal } from './components/Modal.js';
-export { Card } from './components/Card.js';
-export { Button } from './components/Button.js';
-export { ThemeSwitcher } from './ThemeSwitcher.js';
--- packages/utils/src/index.js
+++
@@ -1,7 +0,0 @@
-// @yavl/utils - Index de exports principales
-// Utilidades compartidas del ecosistema Yavl
-
-export * from './formatters.js';
-export * from './validators.js';
-export * from './dateUtils.js';
-export * from './constants.js';
